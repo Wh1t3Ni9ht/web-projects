9f3872a766601be1a27b35411adbee229bf9b270
wh1t3 ni9ht <wh1t3ni9ht@gmail.com>
2020-01-17 22:37:49 +0300

    Rearranged function location and removed false from return statement.

--- Age calculator/main.js
+++ Age calculator/main.js
@@ -14,6 +14,50 @@ let userYear = document.getElementById('year-input');
 let userMonth = document.getElementById('month-input');
 let userDate = document.getElementById('date-input');
 
+function validateDates(){
+	let errMsg = document.getElementById('error-msg');
+
+	if (userYear.value > year || userYear.value < 1) {
+		userYear.classList.remove('is-warning');
+		userYear.classList.add('is-danger');
+		errMsg.innerHTML = '<div class="column is-one-quarter is-offset-5 has-text-centered has-background-danger has-text-white">'+'Invalid year'+'</div>';
+		chkBtn.setAttribute('disabled', '');
+		return
+	}
+	
+	if (userMonth.value > 12 || userMonth.value < 1) {
+		userMonth.classList.remove('is-warning');
+		userMonth.classList.add('is-danger');
+		errMsg.innerHTML = 'Invalid month';
+		errMsg.innerHTML = '<div class="column is-one-quarter is-offset-5 has-text-centered has-background-danger has-text-white">'+'Invalid month'+'</div>';
+		chkBtn.setAttribute('disabled', '');
+		return
+	}
+
+	if (userDate.value > 31 || userDate.value < 1) {
+		userDate.classList.remove('is-warning');
+		userDate.classList.add('is-danger');
+		errMsg.innerHTML = 'Invalid date';
+		errMsg.innerHTML = '<div class="column is-one-quarter is-offset-5 has-text-centered has-background-danger has-text-white">'+'Invalid date'+'</div>';
+		chkBtn.setAttribute('disabled', '');
+		return
+	}
+
+	// Year css style
+	userYear.classList.remove('is-danger');
+	userYear.classList.add('is-warning');
+	// Month css style
+	userMonth.classList.remove('is-danger');
+	userMonth.classList.add('is-warning');
+	// Date css style
+	userDate.classList.remove('is-danger');
+	userDate.classList.add('is-warning');
+	
+	errMsg.innerHTML = '';
+	
+	chkBtn.removeAttribute('disabled', '')
+}
+
 function checkAge() {
 	let result;
 	const display = document.getElementById('result');
@@ -36,8 +80,11 @@ function checkAge() {
 			result = year - userYear.value;
 			display.innerHTML = 'Happy Birthday!!!' + '<br />' + "You are " + result + " years old" ;
 		}
+		return
+	}
+	
 	// if user's birth month is greater than today's month
-	} else if (userMonth.value > month) {
+	if (userMonth.value > month) {
 		// it means his birthday did'nt come so  he is still the same age, no matter what the date is.
 		result = year - userYear.value - 1;
 		display.innerHTML = "You are " + result + " years old";
@@ -47,50 +94,4 @@ function checkAge() {
 		result = year - userYear.value;
 		display.innerHTML = "You are " + result + " years old";
 	}
-}
-
-function validateDates(){
-	let errMsg = document.getElementById('error-msg');
-
-	if (userYear.value > year || userYear.value < 1) {
-		userYear.classList.remove('is-warning');
-		userYear.classList.add('is-danger');
-		errMsg.innerHTML = '<div class="column is-one-quarter is-offset-5 has-text-centered has-background-danger has-text-white">'+'Invalid year'+'</div>';
-		chkBtn.setAttribute('disabled', '');
-		return false
-	}
-	
-	if (userMonth.value > 12 || userMonth.value < 1) {
-		userMonth.classList.remove('is-warning');
-		userMonth.classList.add('is-danger');
-		errMsg.innerHTML = 'Invalid month';
-		errMsg.innerHTML = '<div class="column is-one-quarter is-offset-5 has-text-centered has-background-danger has-text-white">'+'Invalid month'+'</div>';
-		chkBtn.setAttribute('disabled', '');
-		return false
-	}
-
-	if (userDate.value > 31 || userDate.value < 1) {
-		userDate.classList.remove('is-warning');
-		userDate.classList.add('is-danger');
-		errMsg.innerHTML = 'Invalid date';
-		errMsg.innerHTML = '<div class="column is-one-quarter is-offset-5 has-text-centered has-background-danger has-text-white">'+'Invalid date'+'</div>';
-		chkBtn.setAttribute('disabled', '');
-		return false
-	}
-
-	
-	// Year css style
-	userYear.classList.remove('is-danger');
-	userYear.classList.add('is-warning');
-	// Month css style
-	userMonth.classList.remove('is-danger');
-	userMonth.classList.add('is-warning');
-	// Date css style
-	userDate.classList.remove('is-danger');
-	userDate.classList.add('is-warning');
-	
-	errMsg.innerHTML = '';
-	
-	chkBtn.removeAttribute('disabled', '')
-	return true
 }
