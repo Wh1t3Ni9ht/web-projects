65022f37c99f854f33d32b1c69a4fd58a2fe60e7
wh1t3 ni9ht <wh1t3ni9ht@gmail.com>
2020-01-17 22:29:40 +0300

    Improve code and remove unnecassary code

--- Age calculator/main.js
+++ Age calculator/main.js
@@ -1,92 +1,96 @@
-var button = document.getElementById('button');
-button.addEventListener('click', check);
+'use strict'
 
-var main = document.getElementById('main');
-main.addEventListener('input', validDates);
+const chkBtn = document.getElementById('check-age-btn');
+chkBtn.addEventListener('click', checkAge);
+
+const inputValidation = document.getElementById('main');
+inputValidation.addEventListener('input', validateDates);
 
 // Globals
-var result;
-var display = document.getElementById('result');
-var notification = document.getElementById('notification');
-var D = new Date();
-var month = D.getMonth() + 1;
-var year = D.getFullYear();
-var date = D.getDate();
-var yearValue = document.getElementById('yearValue');
-var monthValue = document.getElementById('monthValue');
-var dateValue = document.getElementById('dateValue');
+const month = new Date().getMonth() + 1;
+const year = new Date().getFullYear();
+const date = new Date().getDate();
+let userYear = document.getElementById('year-input');
+let userMonth = document.getElementById('month-input');
+let userDate = document.getElementById('date-input');
 
-function check(e) {
-	var yearValue = document.getElementById('yearValue');
-	var monthValue = document.getElementById('monthValue');
-	var dateValue = document.getElementById('dateValue');
+function checkAge() {
+	let result;
+	const display = document.getElementById('result');
+	// Notification css style
+	const notification = document.getElementById('notification');
+	notification.classList.add('notification');
 
-	if (yearValue.classList.contains('is-danger')) {
-		alert("Invalid year");
-	} else if (monthValue.classList.contains('is-danger')) {
-		alert('Invalid month');
-	} else if (dateValue.classList.contains('is-danger')) {
-		alert('Invalid date');
-	} else {
-		var yearValue = document.getElementById('yearValue').value;
-		var monthValue = document.getElementById('monthValue').value;
-		var dateValue = document.getElementById('dateValue').value;
-
-		notification.classList.add('notification');
-		// Check if user's birth month is equal to today's month
-		if (monthValue == month) {
-			// Check if user's birth date is greater than today's date
-			if (dateValue > date) {
-				// If birth date is greater, it means his birthday did'nt come so  he is still the same age
-				result = year - yearValue - 1;
-				display.innerHTML = "You are " + result + " years old";
-			} else {
-				// If birth date is less, it means his birthday came so he is one year older
-				result = year - yearValue;
-				display.innerHTML = "You are " + result + " years old";
-			}
-			// if user's birth month is greater than today's month
-		} else if (monthValue > month) {
-			// it means his birthday did'nt come so  he is still the same age, no matter what the date is.
-			result = year - yearValue - 1;
+	// Check if user's birth month is equal to current month
+	if (userMonth.value == month) {
+		// Check if user's birth date is greater than today's date
+		if (userDate.value > date) {
+			// If birth date is greater, it means his birthday did not come so  he is still the same age
+			result = year - userYear.value - 1;
 			display.innerHTML = "You are " + result + " years old";
-			// if user's birth month is less than today's month
-		} else{
-			// it means his birthday came so he is one year older, no matter what the date is.
-			result = year - yearValue;
+		} else if (userDate.value < date) {
+			// If birth date is less, it means his birthday came so he is one year older
+			result = year - userYear.value;
 			display.innerHTML = "You are " + result + " years old";
+		} else {
+			result = year - userYear.value;
+			display.innerHTML = 'Happy Birthday!!!' + '<br />' + "You are " + result + " years old" ;
 		}
+	// if user's birth month is greater than today's month
+	} else if (userMonth.value > month) {
+		// it means his birthday did'nt come so  he is still the same age, no matter what the date is.
+		result = year - userYear.value - 1;
+		display.innerHTML = "You are " + result + " years old";
+		// if user's birth month is less than today's month
+	} else {
+		// it means his birthday came so he is one year older, no matter what the date is.
+		result = year - userYear.value;
+		display.innerHTML = "You are " + result + " years old";
 	}
 }
 
-function validDates(){
-	if (yearValue.value > year) {
-		yearValue.classList.remove('is-warning');
-		yearValue.classList.add('is-danger');
-	} else {
-		if (yearValue.classList.contains('is-danger')) {
-			yearValue.classList.remove('is-danger');
-			yearValue.classList.add('is-warning');
-		}
+function validateDates(){
+	let errMsg = document.getElementById('error-msg');
+
+	if (userYear.value > year || userYear.value < 1) {
+		userYear.classList.remove('is-warning');
+		userYear.classList.add('is-danger');
+		errMsg.innerHTML = '<div class="column is-one-quarter is-offset-5 has-text-centered has-background-danger has-text-white">'+'Invalid year'+'</div>';
+		chkBtn.setAttribute('disabled', '');
+		return false
+	}
+	
+	if (userMonth.value > 12 || userMonth.value < 1) {
+		userMonth.classList.remove('is-warning');
+		userMonth.classList.add('is-danger');
+		errMsg.innerHTML = 'Invalid month';
+		errMsg.innerHTML = '<div class="column is-one-quarter is-offset-5 has-text-centered has-background-danger has-text-white">'+'Invalid month'+'</div>';
+		chkBtn.setAttribute('disabled', '');
+		return false
 	}
 
-	if (monthValue.value > 12) {
-		monthValue.classList.remove('is-warning');
-		monthValue.classList.add('is-danger');
-	} else {
-		if (monthValue.classList.contains('is-danger')) {
-			monthValue.classList.remove('is-danger');
-			monthValue.classList.add('is-warning');
-		}
+	if (userDate.value > 31 || userDate.value < 1) {
+		userDate.classList.remove('is-warning');
+		userDate.classList.add('is-danger');
+		errMsg.innerHTML = 'Invalid date';
+		errMsg.innerHTML = '<div class="column is-one-quarter is-offset-5 has-text-centered has-background-danger has-text-white">'+'Invalid date'+'</div>';
+		chkBtn.setAttribute('disabled', '');
+		return false
 	}
 
-	if (dateValue.value > 31) {
-		dateValue.classList.remove('is-warning');
-		dateValue.classList.add('is-danger');
-	} else {
-		if (dateValue.classList.contains('is-danger')) {
-			dateValue.classList.remove('is-danger');
-			dateValue.classList.add('is-warning');
-		}
-	}
+	
+	// Year css style
+	userYear.classList.remove('is-danger');
+	userYear.classList.add('is-warning');
+	// Month css style
+	userMonth.classList.remove('is-danger');
+	userMonth.classList.add('is-warning');
+	// Date css style
+	userDate.classList.remove('is-danger');
+	userDate.classList.add('is-warning');
+	
+	errMsg.innerHTML = '';
+	
+	chkBtn.removeAttribute('disabled', '')
+	return true
 }
